a758ad3cc9c43bb19c8c1f34a045ab6fd7d67349
onekam <onekam@mail.gvsu.edu>
2016-03-17 19:42:10 -0400

    created dictionary activity, made it so choose term activity can go to other activities

--- app/src/main/java/kmcbinc/gvdirections/ChooseTaskActivity.java
+++ app/src/main/java/kmcbinc/gvdirections/ChooseTaskActivity.java
@@ -1,7 +1,11 @@
 package kmcbinc.gvdirections;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ChooseTaskActivity extends AppCompatActivity {
 
@@ -9,5 +13,50 @@ public class ChooseTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_task);
+    }
+
+    class Button_Clicker implements Button.OnClickListener
+    {
+        @Override
+        public void onClick(View v) {
+
+            // if the user clicks
+            if(((TextView) v).getText().toString().equals("whatIsBuilding"))
+            {
+                // Toast.makeText(v.getContext(), "Hello!! button Clicked", Toast.LENGTH_SHORT).show();
+
+                Intent launchme = new Intent(ChooseTaskActivity.this, MainActivity.class);
+
+                ChooseTaskActivity.this.startActivity(launchme);
+                startActivity (launchme);
+            }
+            else if(((TextView) v).getText().toString().equals("howToGetHere"))
+            {
+                // Toast.makeText(v.getContext(), "Hello!! button Clicked", Toast.LENGTH_SHORT).show();
+
+                Intent launchme = new Intent(ChooseTaskActivity.this, MainActivity.class);
+
+                ChooseTaskActivity.this.startActivity(launchme);
+                startActivity (launchme);
+            }
+            else if(((TextView) v).getText().toString().equals("whereCanFind"))
+            {
+                // Toast.makeText(v.getContext(), "Hello!! button Clicked", Toast.LENGTH_SHORT).show();
+
+                Intent launchme = new Intent(ChooseTaskActivity.this, ChooseTermActivity.class);
+
+                ChooseTaskActivity.this.startActivity(launchme);
+                startActivity (launchme);
+            }
+            else if(((TextView) v).getText().toString().equals("whatDoesThisMean"))
+            {
+                // Toast.makeText(v.getContext(), "Hello!! button Clicked", Toast.LENGTH_SHORT).show();
+
+                Intent launchme = new Intent(ChooseTaskActivity.this, DictionaryActivity.class);
+
+                ChooseTaskActivity.this.startActivity(launchme);
+                startActivity (launchme);
+            }
+        }
     }
 }
